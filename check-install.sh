48fc5deffe5320525c139fd181b1a6418237d8ee
amonteilhet <amonteilhet.ext@orange.com>
2016-12-12 19:01:28 +0100

    add colored msg in check scripts

--- check-install.sh
+++ check-install.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+RED="\x1b[1;31m"
+GREEN="\x1b[1;32m"
+DEF="\x1b[0m"
+
 
 ### run this script to check if click is installed
 
@@ -8,15 +12,31 @@ find /usr/local/sbin/ -name "click*"
 find /usr/local/lib/ -name "*.ko"
 printf "\n"
 click-install --version
+if [ $? -ne 0 ]; then
+  printf "\n${RED} click in kernel mode is not installed${DEF}\n"
+else
+  printf "\n${GREEN} click in kernel mode is installed${DEF}\n"
+fi
 
 printf "\nClick User-level application\n"
 printf "============================\n"
 find /usr/local/bin/ -name "click*"
 printf "\n"
 click --version
+if [ $? -ne 0 ]; then
+  printf "\n${RED} click in user mode is not installed${DEF}\n"
+else
+  printf "\n${GREEN} click in user mode is installed${DEF}\n"
+fi
+
 
 printf "\nClicky tool\n"
 printf "============================\n"
 find /usr/local/bin/ -name "clicky*"
 printf "\n"
 clicky --version
+if [ $? -ne 0 ]; then
+  printf "\n${RED} clicky tool is not installed${DEF}\n"
+else
+  printf "\n${GREEN} clicky tool is installed${DEF}\n"
+fi
--- check-kernel.sh
+++ check-kernel.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+RED="\x1b[1;31m"
+BROWN="\x1b[0;33m"
+DEF="\x1b[0m"
+
+
 
 ### run this script to check if a kernel click configuration is installed
 
@@ -8,6 +13,10 @@ find /usr/local/sbin/ -name "click*"
 find /usr/local/lib/ -name "*.ko"
 printf "\n"
 click-install --version
+if [ $? -ne 0 ]; then
+  printf "\n${RED} click in kernel mode is not installed${DEF}\n"
+  exit 1
+fi
 
 ### if click kernel driver is running => :
 ### - click module is loaded
@@ -19,7 +28,7 @@ click-install --version
 printf "\nCheck Click Kernel module\n"
 if [[ `lsmod | grep click` ]] ; then
   printf "> click module is loaded\n"
-else printf "click is not running at kernel\n" ; exit 1; fi
+else printf "${BROWN} click is not running at kernel${DEF}\n" ; exit 1; fi
 
 if [[ `ls /proc | grep click` ]] ; then
   printf "> /proc/click symbolic link exists and points to /click\n"
